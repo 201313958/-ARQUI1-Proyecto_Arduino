98f4b133b79d52c5ad85495b188cccef5e98ea46
Jose Pablo Valiente <2352710550101@ingenieria.usac.edu.gt>
2021-03-07 20:58:01 -0600

    Version final, lista para la entrega

--- Codigo/Codigo.ino
+++ Codigo/Codigo.ino
@@ -4,6 +4,28 @@
 #include <Adafruit_GFX.h>
 #include <Max72xxPanel.h>
 
+//Variables de temperatura
+int senLM35 = A7;
+float temp;
+float vt;
+int rojo = 13;
+int amarillo = 12;
+int verde = 11;
+float tempAnterior = 0;
+float temperatura;
+bool flag_emergencia = false;
+
+//Variables para el reloj
+byte digitOne[10] = {B01000000, B01111001, B00100100, B00110000, B00011001, B00010010, B00000010, B01111000, B00000000, B00010000}; //digitos del 0 al 9
+byte contador3 = 0; //Centenas
+byte contador = 0; //decenas
+byte contador2 = 0; //unidades
+unsigned long tiempo1 = 0;
+unsigned long tiempo2 = 0;
+unsigned long tiempo3 = 0;
+String value;
+
+
 //crear un objeto LiquidCrystal (rs,enable,d4,d5,d6,d7)
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
@@ -51,13 +73,33 @@ String estacion_actual;
 bool flag_parcearCadena = false;
 int contador_estaciones = 0;
 char caracter;
+char cinta;
 char caracter_emergencia;
 bool estado_emergencia = false;
+
+String texto_fila = "ACYE 1 G2";
 
 void setup() {
   Serial.begin(9600);
   Serial1.begin(9600);
-  
+
+
+  //Variables del reloj
+  DDRK = B11111111; //declaramos como salida
+  DDRL = B11111111;
+  DDRF = B01111111;
+  tiempo1 = millis();
+  PORTK = digitOne[0];
+  PORTL = digitOne[0];
+  PORTF = digitOne[0];
+
+  //Variables de temperatura
+  pinMode(verde, OUTPUT);
+  pinMode(amarillo, OUTPUT);
+  pinMode(rojo, OUTPUT);
+  pinMode(senLM35, INPUT);
+
+
   //Motores Stepper
   pinMode(29, OUTPUT);
   pinMode(30, OUTPUT);
@@ -97,22 +139,65 @@ void setup() {
 
 }
 
+void Mensaje_Mov() {
+  int tam_texto = texto_fila.length();
+  
+  for (int i = tam_texto; i > 0 ; i--)
+  {
+    String texto = texto_fila.substring(i - 1);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(texto);
+    delay(75);
+  }
+
+  for (int i = 1; i <= 16; i++)
+  {
+    lcd.clear();
+    lcd.setCursor(i, 0);
+    lcd.print(texto_fila);
+    delay(75);
+  }
+}
+
 void Estacion() {
   if (Serial.available()) {
     caracter = Serial.read();
     if (caracter == '1') {
       estacion_actual = "1";
+      lcd.clear();
+      lcd.setCursor(1, 0);
+      lcd.print("  Estacion 1   ");
+      lcd.setCursor(1, 1);
+      lcd.print("------------------");
     } else if (caracter == '2') {
       estacion_actual = "2";
+      lcd.clear();
+      lcd.setCursor(1, 0);
+      lcd.print("  Estacion 2   ");
+      lcd.setCursor(1, 1);
+      lcd.print("------------------");
     } else if (caracter == '3') {
+      lcd.clear();
+      lcd.setCursor(1, 0);
+      lcd.print("  Estacion 3   ");
+      lcd.setCursor(1, 1);
+      lcd.print("------------------");
       estacion_actual = "3";
     } else if (caracter == '4') {
       estado_emergencia = true;
       Serial1.println("EMERGENCIA!");
+    } else if (caracter == '5') {
+      cinta = '5';
+      Serial1.println("StepperOn: " + (String) cinta);
+    } else if (caracter == '6') {
+      cinta = '6';
+      Serial1.println("StepperOff: " + (String) cinta);
     }
   } else {
     estacion_actual = "error";
   }
+  reloj();
 }
 
 String Recibiendo_Cadena() {
@@ -131,7 +216,7 @@ String Recibiendo_Cadena() {
 void loop() {
   switch (estado) {
     case 1:
-      stepper();
+      //Mensaje_Mov();
       USUARIO();
       delay(50);
       break;
@@ -141,8 +226,16 @@ void loop() {
       if (flag_parcearCadena == true) {
         parcearCadena(cadena);
       }
+      PORTK = PORTL = PORTF = digitOne[0];
+      contador = contador2 = contador3 = 0;
+      lcd.clear();
+      lcd.setCursor(1, 0);
+      lcd.print("  ACYE 1 G2   ");
+      lcd.setCursor(1, 1);
+      lcd.print("------------------");
       break;
     case 3:
+      stepper();
       Estacion();
       if (estado_emergencia == true) {
         estado_emergencia = false;
@@ -173,12 +266,20 @@ void loop() {
           matrizLED.write();
         }
       }
+      reloj();
+      Temperatura();
+      if (flag_emergencia) {
+        Serial.println("PELIGRO");
+      } else {
+        Serial.println("Temperatura: " + (String) temperatura + "C" + "      " + value);
+      }
+      break;
+    case 4:
       break;
   }
 }
 
 void USUARIO() {
-  lcd.clear();
   lcd.setCursor(1, 0);
   lcd.print("  INGRESE SU    ");
   lcd.setCursor(1, 1);
@@ -410,6 +511,11 @@ void imprimirPanel4(int numero) {
     matrizLED.write();
     Serial1.println("Limpia Valores y Resetea");
   }
+  if (flag_emergencia) {
+    Serial.println("PELIGRO");
+  } else {
+    Serial.println("Temperatura: " + (String) temperatura + "C" + "      " + value);
+  }
   switch (numero) {
     case 1: matrizLED.drawChar(x, y, '1', HIGH, LOW, 1);
       matrizLED.write();
@@ -440,7 +546,17 @@ void imprimirPanel4(int numero) {
           Serial1.println("Limpia Valores y Resetea");
           break;
         }
+        if (flag_emergencia) {
+          Serial.println("PELIGRO");
+        } else {
+          Serial.println("Temperatura: " + (String) temperatura + "C" + "      " + value);
+        }
       }
+      lcd.clear();
+      lcd.setCursor(1, 0);
+      lcd.print("  ACYE 1 G2   ");
+      lcd.setCursor(1, 1);
+      lcd.print("------------------");
       verificarTopar();
 
 
@@ -475,7 +591,17 @@ void imprimirPanel4(int numero) {
           Serial1.println("Limpia Valores y Resetea");
           break;
         }
+        if (flag_emergencia) {
+          Serial.println("PELIGRO");
+        } else {
+          Serial.println("Temperatura: " + (String) temperatura + "C" + "      " + value);
+        }
       }
+      lcd.clear();
+      lcd.setCursor(1, 0);
+      lcd.print("  ACYE 1 G2   ");
+      lcd.setCursor(1, 1);
+      lcd.print("------------------");
       verificarTopar();
       //verificarTopar();
       break;
@@ -508,7 +634,17 @@ void imprimirPanel4(int numero) {
           Serial1.println("Limpia Valores y Resetea");
           break;
         }
+        if (flag_emergencia) {
+          Serial.println("PELIGRO");
+        } else {
+          Serial.println("Temperatura: " + (String) temperatura + "C" + "      " + value);
+        }
       }
+      lcd.clear();
+      lcd.setCursor(1, 0);
+      lcd.print("  ACYE 1 G2   ");
+      lcd.setCursor(1, 1);
+      lcd.print("------------------");
       verificarTopar();
       //verificarTopar();
       break;
@@ -570,24 +706,94 @@ void serial_Estacion(String recibir) {
 }
 
 void stepper() {
-  digitalWrite(29, HIGH);
-  digitalWrite(30, LOW);
-  digitalWrite(31, LOW);
-  digitalWrite(32, LOW);
-  delay(180);
-  digitalWrite(29, LOW);
-  digitalWrite(30, HIGH);
-  digitalWrite(31, LOW);
-  digitalWrite(32, LOW);
-  delay(180);
-  digitalWrite(29, LOW);
-  digitalWrite(30, LOW);
-  digitalWrite(31, HIGH);
-  digitalWrite(32, LOW);
-  delay(180);
-  digitalWrite(29, LOW);
-  digitalWrite(30, LOW);
-  digitalWrite(31, LOW);
-  digitalWrite(32, HIGH);
-  delay(180);
+
+  if (cinta == '5') {
+    lcd.clear();
+    lcd.setCursor(1, 0);
+    lcd.print("CINTA TRANSPORTA");
+    lcd.setCursor(1, 1);
+    lcd.print("  MOVIENDOSE  ");
+    digitalWrite(29, HIGH);
+    digitalWrite(30, LOW);
+    digitalWrite(31, LOW);
+    digitalWrite(32, LOW);
+    delay(180);
+    digitalWrite(29, LOW);
+    digitalWrite(30, HIGH);
+    digitalWrite(31, LOW);
+    digitalWrite(32, LOW);
+    delay(180);
+    digitalWrite(29, LOW);
+    digitalWrite(30, LOW);
+    digitalWrite(31, HIGH);
+    digitalWrite(32, LOW);
+    delay(180);
+    digitalWrite(29, LOW);
+    digitalWrite(30, LOW);
+    digitalWrite(31, LOW);
+    digitalWrite(32, HIGH);
+    delay(180);
+  }
+}
+
+void reloj() {
+  tiempo2 = millis(); //tiempo trasncurrido
+  if (tiempo2 > (tiempo1 + 350)) { // indica que ha pasado 1 segundo de tiempo en arduino
+    value = "Tiempo: " + (String)contador3 + ":" + (String)contador + "" + (String) contador2; //centenas, decenas , unidades
+    //Serial.println(value); //lo mandamos por puerto serial
+    tiempo1 = millis(); //actualizamos el tiempo 1
+    PORTK =  digitOne[contador2]; //escribimos las unidades
+    contador2 = contador2 + 1;
+
+    if (contador2 == 10 ) {
+      contador2 = 0; //volvemos a 0 las unidades
+      contador = contador + 1; //aumentamos decenas
+    }
+  }
+  if (contador2 == 1) {
+    PORTL = digitOne[contador];
+    if (contador == 6) { //si ya llegó a 6 las decenas, ya pasó 1 minuto, y hay que volver a iniciar
+      contador = 0;
+      contador3++;
+    }
+    PORTF = digitOne[contador3];
+  }
+}
+
+
+void Temperatura() {
+  vt = analogRead(senLM35);
+  //El sensor nos devuelve de 0 a 1.5V
+  float mv = (vt / 1024.0) * 5000; //miliVoltios
+  temperatura = mv / 10; //temperatura en grados celsius
+
+  if (temperatura < 37) {
+    digitalWrite(verde, HIGH); //Enciende el verde
+    digitalWrite(amarillo, LOW); //Se apaga el amarillo
+    digitalWrite(rojo, LOW); //Se apaga el rojo
+    lcd.setCursor(1, 0);
+    lcd.print("  ACYE 1 G2   ");
+    lcd.setCursor(1, 1);
+    lcd.print("------------------");
+    flag_emergencia = false;
+  } else if (temperatura > 36 && temperatura < 46) {
+    digitalWrite(verde, LOW); //Se apaga el verde
+    digitalWrite(amarillo, HIGH); //Enciende el amarillo
+    digitalWrite(rojo, LOW); //Se apaga el rojo
+    lcd.setCursor(1, 0);
+    lcd.print("  WARNING   ");
+    lcd.setCursor(1, 1);
+    lcd.print("------------------");
+    flag_emergencia = false;
+  } else if (temperatura > 45) {
+    digitalWrite(verde, LOW); //Se apaga el verde
+    digitalWrite(amarillo, LOW); //Se apaga el amarillo
+    digitalWrite(rojo, HIGH); //Enciende el rojo
+    flag_emergencia = true;
+    lcd.setCursor(1, 0);
+    lcd.print("  DANGER   ");
+    lcd.setCursor(1, 1);
+    lcd.print("------------------");
+    Serial.println("PELIGRO");
+  }
 }
